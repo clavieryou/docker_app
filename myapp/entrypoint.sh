1c7b20eda17a00ecd13bff8ddef420008b60a312
youclavier <james.you@crypto.com>
2020-05-22 23:32:50 +0800

    update

--- myapp/entrypoint.sh
+++ myapp/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
 # rm -f /myapp/tmp/pids/server.pid
-cd /home/app
+cd /home/myapp
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 bundle exec puma -C config/puma.rb
 bundle exec rails db:migrate
